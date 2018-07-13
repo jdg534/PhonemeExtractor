64315f6933afcef22c9ded2ba98d47605dfd56bc
Josh Gibson <joshdgibson21@outlook.com>
2018-07-13 21:43:39 +0100

    Added relevent .sh files

---
+++ mkCodeLiteNinjaDevEnv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# make the DEV_ENV directory
+# cd into DEV_ENV
+# call CMake starting with the directory that this file sits in, with target output of CodeBlocks with Unix Makefiles
+
+mkdir DEV_ENV
+cd DEV_ENV
+cmake .. -G "CodeLite - Ninja"
+
+
